0198af26cd1b2230e6da5f75a766b4f64ac29703
prathameshvattamwar <prathamesh.vattamwar24@gmail.com>
2024-10-26 03:46:46 +0530

    Added all files

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    profile_image = db.Column(db.String(200), nullable=True)  # Field to store the profile image filename
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    profile_image = db.Column(db.String(200), nullable=True)  # Store profile image with the message
